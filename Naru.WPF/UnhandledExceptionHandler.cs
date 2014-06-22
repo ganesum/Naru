39be836601cbfe5358fd60d9440470e7d3343b0d
Peter Strong <xjpeter@gmail.com>
2014-06-22 12:01:32 +0100

    Code tidyup.

--- Naru.WPF/UnhandledExceptionHandler.cs
+++ Naru.WPF/UnhandledExceptionHandler.cs
@@ -6,17 +6,22 @@ namespace Naru.WPF
 {
     public static class UnhandledExceptionHandler
     {
-        private static readonly ILog Logger = LogManager.GetLogger(typeof(Naru.TPL.UnhandledExceptionHandler));
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Naru.WPF.UnhandledExceptionHandler));
 
         public static void InstallDispatcherUnhandledException()
         {
+            Logger.Debug("Installing DispatcherUnhandledException logging");
+
             Dispatcher.CurrentDispatcher.UnhandledException += CurrentDispatcher_UnhandledException;
         }
 
         public static void UninstallDispatcherUnhandledException()
         {
+            Logger.Debug("Uninstalling DispatcherUnhandledException logging");
+
             Dispatcher.CurrentDispatcher.UnhandledException -= CurrentDispatcher_UnhandledException;
         }
+
         private static void CurrentDispatcher_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
             Logger.Error(e.Exception);
